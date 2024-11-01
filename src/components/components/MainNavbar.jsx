4a24674d1512d283565d5dbeff7cd83eb100ba8b
Jyun Rong Liu <jyunrl@uci.edu>
2024-10-31 17:40:02 -0700

    implemented nav bar ui

--- src/components/components/MainNavbar.jsx
+++ src/components/components/MainNavbar.jsx
@@ -1,34 +1,68 @@
-import { Flex, Text, Button, Icon, HStack } from "@chakra-ui/react";
-import { HiSquare3Stack3D } from "react-icons/hi2";
+import { Flex, Text, Button, Icon, HStack, Box } from "@chakra-ui/react";
 
 export default function MainNavBar() {
-  return (
-    <Flex flexDir={"column"}>
-      <Flex
-        justifyContent={"space-between"}
-        padding={"1rem"}
-        marginBottom={"-3px"}
-      >
-        <HStack fontSize={"18px"}>
-          <Icon>
-            <HiSquare3Stack3D></HiSquare3Stack3D>
-          </Icon>
-          <Text fontWeight={"bold"}>Orgnization</Text>
-        </HStack>
+  const navPage = ["About", "Projects", "Jobs", "Transactions"];
 
-        <HStack gap={"20px"}>
-          <Button>placeholder</Button>
-          <Button>placeholder</Button>
-        </HStack>
+  return (
+    <Flex backgroundColor={"white"} height={"8vh"} gap={"20vh"}>
+      <Flex gap={"10px"} padding={"0.5rem"} marginLeft={"40vh"}>
+        {navPage.map((page) => {
+          return (
+            <Button
+              color={"black"}
+              backgroundColor={"transparent"}
+              _hover={{
+                color: "gray",
+              }}
+              fontSize={"20px"}
+            >
+              {page}
+            </Button>
+          );
+        })}
       </Flex>
-      <hr
-        style={{
-          backgroundColor: "#383838",
-          height: "2px",
-          width: "100%",
-        }}
-        class="solid"
-      />
+
+      <Flex padding={"0.5rem"} alignItems={"center"}>
+        <Button
+          color={"black"}
+          backgroundColor={"transparent"}
+          _hover={{
+            color: "gray",
+          }}
+          fontSize={"20px"}
+        >
+          James
+        </Button>
+        <Box
+          width="1.5px"
+          height="30px"
+          backgroundColor={"black"}
+          marginTop={"1vh"}
+        />
+        <Button
+          color={"black"}
+          backgroundColor={"transparent"}
+          _hover={{
+            color: "gray",
+          }}
+          fontSize={"20px"}
+        >
+          Logout
+        </Button>
+        <Button
+          fontSize={"20px"}
+          backgroundColor={"#497497"}
+          color={"white"}
+          marginLeft={"3vh"}
+          borderRadius={"10vh"}
+          width={"17vh"}
+          height={"5vh"}
+          opacity={"0.8"}
+          _hover={{ opacity: 1.0 }}
+        >
+          Apply
+        </Button>
+      </Flex>
     </Flex>
   );
 }
